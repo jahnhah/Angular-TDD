2aff3f20d31d222f7e663f4f91897e72aacf6b45
jahnhah <95902802+jahnhah@users.noreply.github.com>
2022-10-16 20:11:10 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserRegistrationComponent } from './user/features/user-registration/user-registration.component';
+import { UserAuthenticationComponent } from './user/features/user-authentication/user-authentication.component';
+import { LoginFormComponent } from './user/ui/login-form/login-form.component';
+import { RegistrationFormComponent } from './user/ui/registration-form/registration-form.component';
+import { VerificationFormComponent } from './user/ui/verification-form/verification-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrationComponent,
+    UserAuthenticationComponent,
+    LoginFormComponent,
+    RegistrationFormComponent,
+    VerificationFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
